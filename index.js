0f23d322319b44c331b5e2956e19fdf7e7cce484
Mitchell Glass <mitchell.glass@valpo.edu>
2019-07-12 05:42:04 -0500

    Add keyboard support

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function enter(number) {
+    if(number === '.') {
+        if (current.includes('.')) {
+            return;
+        }
+    }
     current += number;
     display(current);
 }
 
 function display(disp) {
-    result.textContent = disp;
+    console.log('current: ' + current);
+    if(current.length > 0) {
+        result.textContent = disp;
+    }
 }
 
 function invert() {
@@ -12,8 +20,15 @@ function invert() {
     display(current);
 }
 
+function del() {
+    current = current.substring(0, current.length - 1);
+    display(current);
+}
+
 function compute() {
     let result = 0;
+    console.log('computing');
+    console.log(firstNumber + " " + current + " " + operation);
     switch(operation) {
         case -1:
             return;
@@ -32,6 +47,7 @@ function compute() {
     }
     firstNumber = numberWithCommas(firstNumber);
     display(firstNumber);
+    console.log('result: ' + firstNumber);
 }
 
 function numberWithCommas(x) {
@@ -40,10 +56,10 @@ function numberWithCommas(x) {
 
 function clearResults() {
     cleared = true;
+    display('0');
     current = '';
     firstNumber = '';
     operation = -1;
-    display('0');
 }
 
 // -1 - initialized value
@@ -107,4 +123,73 @@ let current = '';
 let firstNumber = '';
 let operation = -1;
 let result = document.getElementById('result');
-console.log('current: ' + current);
+
+
+var map = {}; 
+onkeydown = onkeyup = function(e){
+    e = e || event; // to deal with IE
+    map[e.keyCode] = e.type == 'keydown';
+    if(map[187]) {
+        if(map[16]) {
+            operate(0);
+        } else {
+            compute();
+        }
+    }
+}
+
+
+document.addEventListener('keydown', function(event) {
+    let key = event.keyCode;
+    console.log(key)
+    switch(key) {
+        case 49:
+            enter('1');
+            break;
+        case 50:
+            enter('2');
+            break;
+        case 51:
+            enter('3');
+            break;
+        case 52:
+            enter('4');
+            break;
+        case 53:
+            enter('5');
+            break;
+        case 54:
+            enter('6');
+            break;
+        case 55:
+            enter('7');
+            break;
+        case 56:
+            enter('8');
+            break;
+        case 57:
+            enter('9');
+            break;
+        case 48:
+            enter('0');
+            break;
+        case 191:
+            operate(3);
+            break;
+        case 88:
+            operate(2);
+            break;
+        case 189:
+            operate(1);
+            break;
+        case 13:
+            compute();
+            break;
+        case 190:
+            enter('.');
+            break;
+        case 8:
+            del();
+            break;
+    }
+});
